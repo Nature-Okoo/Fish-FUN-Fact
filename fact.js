fc0bdd87c1fa271838546e2dc7da28e9c793126d
INFINITECAMPUS\oliver.koo <oliver.koo@infinitecampus.com>
2017-06-28 17:40:23 -0500

    working state (unclean). pushed it up here so I can pull it down later from my home computer.

--- fact.js
+++ fact.js
@@ -16,13 +16,33 @@ function getImageInfo(searchTerm, callback, errorCallback) {
             console.log($(response));
             var right_content = $($(response).filter('div#page.container').html()).filter('div#right_content').html();
             console.log($(right_content));
-            var fishName = $(right_content).filter('h1').html();
-            console.log(fishName);
-            var fishInfo = $(right_content).filter('p').html();
-            console.log(fishInfo);
-            console.log($(right_content).has('img').html());
-            var imageSrc = $($(right_content).has('img').html()).filter('img').prop('src');
-            callback(imageSrc, fishName, fishInfo);
+            var fishSources = [];
+            //$(right_content).each(function(){
+                // if ($($(this).find('img')).length !== 0)
+                //     var fish = {}
+                //     var fishName = $(this).find('img').filter('img').prop('alt');
+                //     var imgUrl = $(this).find('img').filter('img').prop('src');
+            //});
+            //console.log($(right_content).find('p'));
+            $(right_content).find('p').each(function(){
+                console.log($(this).html());
+            });
+            // var arrayOfIds = $(right_content).find('*').map(function(){
+            //     console.log($(this).find('img'));
+            // }).get();
+
+            // console.log($(right_content).find('p'));
+            // var fishName = $($(right_content).has('img').html()).filter('img').prop('alt');
+            // //console.log(fishName);
+            // var fishInfo = $(right_content).filter('p').html();
+            // console.log(fishInfo);
+            //console.log($(right_content).has('img').html());
+            // var arraysOfIds = $($(response).filter('div#page.container img').html()).map(function(){
+            //     return this.id;
+            // }).get();
+            // console.log(arraysOfIds);
+            //var imageSrc = $($(right_content).has('img').html()).filter('img').prop('src');
+            //callback(imageSrc, fishName, fishInfo);
         } 
         
     };
@@ -53,8 +73,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
     var fishArray = [
         // 'bigmouthbuffalo',
-        // 'yellowperch',
-        'whitesucker'
+        //'yellowperch',
+        // 'whitesucker',
+        // 'walleye/index',
+        'trout/index'
     ];
     var randomNumber = Math.floor(Math.random()*fishArray.length);
 
